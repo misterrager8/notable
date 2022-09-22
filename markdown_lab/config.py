a42615b77ef7795da4cbef1cdc4d42a5ab203bb2
misterrager8 <chemllei0304@gmail.com>
2022-09-21 22:21:18 -0400

    TWEAK: config debug attr

--- markdown_lab/config.py
+++ markdown_lab/config.py
@@ -5,6 +5,6 @@ import dotenv
 dotenv.load_dotenv()
 
 ENV = os.getenv("env")
-DEBUG = os.getenv("debug")
+DEBUG = os.getenv("debug").lower() == "true"
 BASE_DIR = os.getenv("base_dir")
 FLASK_RUN_PORT = os.getenv("port")
