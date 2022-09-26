afd0119c60c253f1cdda290398fc6897dbf82ee8
misterrager8 <chemllei0304@gmail.com>
2022-09-26 03:42:13 -0400

    FEATURE: webbrowser open option, reimplimenting. click interface

--- markdown_lab/cli.py
+++ markdown_lab/cli.py
@@ -1,8 +1,21 @@
+import webbrowser
+
+import click
+
 from markdown_lab import config, create_app
 
 app = create_app(config)
 
 
-def run():
+@click.group()
+def cli():
     """Launch web interface"""
+    pass
+
+
+@cli.command()
+@click.option("--switch", "-s", is_flag=True)
+def run(switch):
+    if switch:
+        webbrowser.open(f"http://127.0.0.1:{config.FLASK_RUN_PORT}/")
     app.run(port=config.FLASK_RUN_PORT)
--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setuptools.setup(
     name="MarkdownLab",
     long_description=open("README.md").read(),
     license=open("LICENSE.md").read(),
-    entry_points={"console_scripts": ["mdlab=markdown_lab.cli:run"]},
+    entry_points={"console_scripts": ["mdlab=markdown_lab.cli:cli"]},
 )
