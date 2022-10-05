bf7e0d3f1218f4e59260090d5ee52bdd67564a32
misterrager8 <chemllei0304@gmail.com>
2022-10-04 23:10:23 -0400

    REFACTOR: simply to two themes with toggle

--- markdown_lab/static/main.js
+++ markdown_lab/static/main.js
@@ -1,11 +1,20 @@
-$(document).ready(function() {
-    document.documentElement.setAttribute('data-theme', localStorage.getItem('MDLab'));
-});
+$(document).ready(function() {{
+    localStorage.getItem('MDLab') === 'dark' ? setDark() : setLight();
+}});
 
-function changeTheme(theme) {
-    localStorage.setItem('MDLab', theme);
+function setDark() {{
+    localStorage.setItem('MDLab', 'dark');
     document.documentElement.setAttribute('data-theme', localStorage.getItem('MDLab'));
-}
+    $('#dark').show();
+    $('#light').hide();
+}}
+
+function setLight() {{
+    localStorage.setItem('MDLab', 'light');
+    document.documentElement.setAttribute('data-theme', localStorage.getItem('MDLab'));
+    $('#light').show();
+    $('#dark').hide();
+}}
 
 function toggleDiv(divId) {
     $('#' + divId).fadeToggle(150);
