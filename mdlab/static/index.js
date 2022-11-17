de639232df8dd2724f52dd6ba27c2ef61283ca59
misterrager8 <chemllei0304@gmail.com>
2022-11-16 19:08:08 -0500

    MISC: changeable navbars, makeshift formatting fnality

--- mdlab/static/index.js
+++ mdlab/static/index.js
@@ -10,26 +10,26 @@ function toggleDiv(id) {
 function setLight() {
 	document.documentElement.setAttribute('data-theme', 'light');
 	localStorage.setItem('MDLab', 'light');
-	$('#setLight').hide();
-	$('#setDark').show();
+	$('a[id^="setLight"]').hide();
+	$('a[id^="setDark"]').show();
 }
 
 function setDark() {
 	document.documentElement.setAttribute('data-theme', 'dark');
 	localStorage.setItem('MDLab', 'dark');
-	$('#setDark').hide();
-	$('#setLight').show();
+	$('a[id^="setDark"]').hide();
+	$('a[id^="setLight"]').show();
 }
 
 function hideNav() {
-	$('body, .sidenav').addClass('collapsed');
+	$('body, .sidenav, .vertnav').addClass('collapsed');
 	localStorage.setItem('navState', 'collapsed');
 	$('#hideNav').hide();
 	$('#showNav').show();
 }
 
 function showNav() {
-	$('body, .sidenav').removeClass('collapsed');
+	$('body, .sidenav, .vertnav').removeClass('collapsed');
 	localStorage.setItem('navState', 'shown');
 	$('#hideNav').show();
 	$('#showNav').hide();
@@ -45,17 +45,15 @@ function getText(folder, name) {
 		$('#note').html(`
 			<div class="btn-group mb-4">
 				<a class="btn btn-outline-success"><i class="bi bi-save" id="saveBtn"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-type-bold"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-type-italic"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-list-ol"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-list-ul"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-link"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-clock"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-type"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-type-h1"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-card-image"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-code"></i></a>
-				<a class="btn btn-outline-secondary"><i class="bi bi-quote"></i></a>
+				<a onclick="copy2clip('****')" class="btn btn-outline-secondary"><i class="bi bi-type-bold"></i></a>
+				<a onclick="copy2clip('**')" class="btn btn-outline-secondary"><i class="bi bi-type-italic"></i></a>
+				<a onclick="copy2clip('1. ')" class="btn btn-outline-secondary"><i class="bi bi-list-ol"></i></a>
+				<a onclick="copy2clip('- ')" class="btn btn-outline-secondary"><i class="bi bi-list-ul"></i></a>
+				<a onclick="copy2clip('[title](url)')" class="btn btn-outline-secondary"><i class="bi bi-link"></i></a>
+				<a onclick="copy2clip(new Date())" class="btn btn-outline-secondary"><i class="bi bi-clock"></i></a>
+				<a onclick="copy2clip('# ')" class="btn btn-outline-secondary"><i class="bi bi-type-h1"></i></a>
+				<a onclick="copy2clip('![title](url)')" class="btn btn-outline-secondary"><i class="bi bi-card-image"></i></a>
+				<a onclick="copy2clip('    ')" class="btn btn-outline-secondary"><i class="bi bi-code"></i></a>
 			</div>
 			<textarea id="content" class="form-control form-control-hidden" rows="30" onchange="editNote('${folder}', '${name}')">${data}</textarea>
 			`);
@@ -100,4 +98,8 @@ function deleteNote(folder, name) {
 	}, function(data) {
 		location.reload();
 	});
+}
+
+function copy2clip(text) {
+	navigator.clipboard.writeText(text);
 }
