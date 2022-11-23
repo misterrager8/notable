345158866a053e77c84ae5b0ba046258eb1c8fad
misterrager8 <chemllei0304@gmail.com>
2022-11-23 16:22:54 -0500

    FEATURE: finish formatText fnality

--- mdlab/static/index.js
+++ mdlab/static/index.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
 	localStorage.getItem('navState') === 'collapsed' ? hideNav() : showNav();
 });
 
+var beforeSel;
+var afterSel;
+
 function toggleDiv(id) {
 	$('#' + id).fadeToggle(150);
 }
@@ -45,19 +48,28 @@ function getText(folder, name) {
 		$('#note').html(`
 			<div class="btn-group mb-4">
 				<a onclick="editNote('${folder}', '${name}')" class="btn btn-outline-success"><i class="bi bi-save" id="saveBtn"></i></a>
-				<a onclick="copy2clip('****')" class="btn btn-outline-secondary"><i class="bi bi-type-bold"></i></a>
-				<a onclick="copy2clip('**')" class="btn btn-outline-secondary"><i class="bi bi-type-italic"></i></a>
-				<a onclick="copy2clip('1. ')" class="btn btn-outline-secondary"><i class="bi bi-list-ol"></i></a>
-				<a onclick="copy2clip('- ')" class="btn btn-outline-secondary"><i class="bi bi-list-ul"></i></a>
-				<a onclick="copy2clip('[title](url)')" class="btn btn-outline-secondary"><i class="bi bi-link"></i></a>
-				<a onclick="copy2clip(new Date())" class="btn btn-outline-secondary"><i class="bi bi-clock"></i></a>
-				<a onclick="copy2clip('# ')" class="btn btn-outline-secondary"><i class="bi bi-type-h1"></i></a>
-				<a onclick="copy2clip('![title](url)')" class="btn btn-outline-secondary"><i class="bi bi-card-image"></i></a>
-				<a onclick="copy2clip('    ')" class="btn btn-outline-secondary"><i class="bi bi-code"></i></a>
+				<a onclick="formatText('bold')" class="btn btn-outline-secondary"><i class="bi bi-type-bold"></i></a>
+				<a onclick="formatText('italic')" class="btn btn-outline-secondary"><i class="bi bi-type-italic"></i></a>
+				<a onclick="formatText('numlist')" class="btn btn-outline-secondary"><i class="bi bi-list-ol"></i></a>
+				<a onclick="formatText('bullist')" class="btn btn-outline-secondary"><i class="bi bi-list-ul"></i></a>
+				<a onclick="formatText('link')" class="btn btn-outline-secondary"><i class="bi bi-link"></i></a>
+				<a onclick="formatText('time')" class="btn btn-outline-secondary"><i class="bi bi-clock"></i></a>
+				<a onclick="formatText('heading')" class="btn btn-outline-secondary"><i class="bi bi-type-h1"></i></a>
+				<a onclick="formatText('image')" class="btn btn-outline-secondary"><i class="bi bi-card-image"></i></a>
+				<a onclick="formatText('code')" class="btn btn-outline-secondary"><i class="bi bi-code"></i></a>
+				<a onclick="formatText('capitalize')" class="btn btn-outline-secondary"><i class="bi bi-type"></i></a>
 			</div>
-			<textarea id="content" class="form-control form-control-hidden" rows="30" onchange="editNote('${folder}', '${name}')">${data}</textarea>
+			<textarea autocomplete="off" class="invisible" id="format"></textarea>
+			<textarea onselect="getselection(event)" id="content" class="form-control form-control-hidden" rows="30" onchange="editNote('${folder}', '${name}')">${data}</textarea>
 			`);
 	});
+}
+
+function getselection(event) {
+	var sel = $('#content').val().substring(event.currentTarget.selectionStart, event.currentTarget.selectionEnd);
+	beforeSel = $('#content').val().substring(0, event.currentTarget.selectionStart);
+	afterSel = $('#content').val().substring(event.currentTarget.selectionEnd);
+	$('#format').val(sel);
 }
 
 function getMarkdown(folder, name) {
@@ -120,6 +132,39 @@ function deleteMemo(folder, name) {
 	});
 }
 
-function copy2clip(text) {
-	navigator.clipboard.writeText(text);
+function formatText(type_) {
+	var mid = $('#format').val();
+	switch (type_) {
+		case 'bold':
+			var newMid = `${beforeSel}**${mid}**${afterSel}`;
+			break;
+		case 'italic':
+			var newMid = `${beforeSel}*${mid}*${afterSel}`;
+			break;
+		case 'link':
+			var newMid = `${beforeSel}[${mid}](url)${afterSel}`;
+			break;
+		case 'heading':
+			var newMid = `${beforeSel}# ${mid}${afterSel}`;
+			break;
+		case 'numlist':
+			var newMid = `${beforeSel}1. ${mid}${afterSel}`;
+			break;
+		case 'bullist':
+			var newMid = `${beforeSel}- ${mid}${afterSel}`;
+			break;
+		case 'time':
+			var newMid = `${beforeSel}${mid}${new Date()}${afterSel}`;
+			break;
+		case 'image':
+			var newMid = `${beforeSel}![${mid}](url)${afterSel}`;
+			break;
+		case 'capitalize':
+			var newMid = `${beforeSel}${mid.toUpperCase()}${afterSel}`;
+			break;
+		case 'code':
+			var newMid = `${beforeSel}    ${mid}${afterSel}`;
+	}
+	$('#content').val(newMid);
+	$('#content').trigger('change');
 }
